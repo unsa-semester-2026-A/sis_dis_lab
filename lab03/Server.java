ec95e758043d26fde3ac2dd090f0f76517e656db
mbarrios28 <mbarriosmed@unsa.edu.pe>
2026-05-05 13:02:25 -0500

    Añadiendo atributos y métodos fundamentales

--- lab03/Server.java
+++ lab03/Server.java
@@ -1,4 +1,108 @@
-/*Broadcast: Descomentar
+import java.io.*;
+import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+// el servidor que puede ejecutarse desde consola
+public class Server {
+
+  // un ID unico para cada conexion
+  private static int uniqueId;
+
+  // un ArrayList para mantener la lista de Clientes
+  private ArrayList<ClientThread> al;
+
+  // para mostrar la hora
+  private SimpleDateFormat sdf;
+
+  // numero de puerto para escuchar conexiones
+  private int port;
+
+  // para verificar si el servidor esta en ejecucion
+  private boolean keepGoing;
+
+  // notificacion
+  private String notif = " *** ";
+
+  // constructor que recibe el puerto para escuchar conexiones como parametro
+  public Server(int port) {
+    // el puerto
+    this.port = port;
+
+    // para mostrar hh:mm:ss
+    sdf = new SimpleDateFormat("HH:mm:ss");
+
+    // un ArrayList para mantener la lista de Clientes
+    al = new ArrayList<ClientThread>();
+  }
+
+  public void start() {
+    keepGoing = true;
+
+    // crear socket de servidor y esperar solicitudes de conexion
+    try {
+
+      // el socket usado por el servidor
+      ServerSocket serverSocket = new ServerSocket(port);
+
+      // bucle infinito para esperar conexiones (mientras el servidor este activo)
+      while (keepGoing) {
+        display("Server waiting for Clients on port " + port + ".");
+
+        // aceptar conexion si el cliente la solicita
+        Socket socket = serverSocket.accept();
+
+        // salir si el servidor se detuvo
+        if (!keepGoing)
+          break;
+
+        // si el cliente esta conectado, crear su hilo
+        ClientThread t = new ClientThread(socket);
+
+        // agregar este cliente al arraylist
+        al.add(t);
+        t.start();
+      }
+
+      // intentar detener el servidor
+      try {
+        serverSocket.close();
+
+        for (int i = 0; i < al.size(); ++i) {
+          ClientThread tc = al.get(i);
+          try {
+            // cerrar todos los flujos de datos y el socket
+            tc.sInput.close();
+            tc.sOutput.close();
+            tc.socket.close();
+          } catch (IOException ioE) {
+          }
+        }
+      } catch (Exception e) {
+        display("Exception closing the server and clients: " + e);
+      }
+
+    } catch (IOException e) {
+      String msg = sdf.format(new Date()) + " Exception on new ServerSocket: " + e + "\n";
+      display(msg);
+    }
+  }
+
+  // para detener el servidor
+  protected void stop() {
+    keepGoing = false;
+    try {
+      new Socket("localhost", port);
+    } catch (Exception e) {
+    }
+  }
+
+  // mostrar un evento en la consola
+  private void display(String msg) {
+    String time = sdf.format(new Date()) + " " + msg;
+    System.out.println(time);
+  }
+
   // para enviar un mensaje a todos los Clientes
   private synchronized boolean broadcast(String message) {
 
@@ -68,5 +172,77 @@
     return true;
   }
 
+  // si el cliente envio LOGOUT para salir
+  synchronized void remove(int id) {
+    String disconnectedClient = "";
 
-*/
+    // recorrer la lista hasta encontrar el Id
+    for (int i = 0; i < al.size(); ++i) {
+      ClientThread ct = al.get(i);
+
+      // si se encuentra, eliminarlo
+      if (ct.id == id) {
+        disconnectedClient = ct.getUsername();
+        al.remove(i);
+        break;
+      }
+    }
+
+    broadcast(notif + disconnectedClient + " has left the chat room." + notif);
+  }
+
+  /*
+  * Para ejecutar como aplicacion de consola
+  * > java Server
+  * > java Server portNumber
+  * Si no se especifica el numero de puerto se usa 1500
+  */
+  public static void main(String[] args) {
+
+    // iniciar servidor en el puerto 1500 salvo que se indique otro PortNumber
+    int portNumber = 1500;
+
+    switch (args.length) {
+      case 1:
+        try {
+          portNumber = Integer.parseInt(args[0]);
+        } catch (Exception e) {
+          System.out.println("Invalid port number.");
+          System.out.println("Usage is: > java Server [portNumber]");
+          return;
+        }
+
+      case 0:
+        break;
+
+      default:
+        System.out.println("Usage is: > java Server [portNumber]");
+        return;
+    }
+
+    // crear un objeto server e iniciarlo
+    Server server = new Server(portNumber);
+    server.start();
+  }
+
+  // una instancia de este hilo se ejecuta por cada cliente
+  class ClientThread extends Thread {
+
+    // socket para recibir mensajes del cliente
+    Socket socket;
+    ObjectInputStream sInput;
+    ObjectOutputStream sOutput;
+
+    // id unico (mas facil para desconectar)
+    int id;
+
+    // nombre de usuario del Cliente
+    String username;
+
+    // objeto mensaje para recibir el mensaje y su tipo
+    ChatMessage cm;
+
+    // marca de tiempo
+    String date;
+  }
+}
