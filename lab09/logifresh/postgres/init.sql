bf94d5c8fd9e6c56e08cfb2c0d9919b54e709088
mbarrios28 <mbarriosmed@unsa.edu.pe>
2026-06-15 22:29:16 -0500

    Agregando inicialización de repositorio definiendo pom y dockercompose

---
+++ lab09/logifresh/postgres/init.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE logifresh_pedidos;
+CREATE DATABASE logifresh_inventario;
+CREATE DATABASE logifresh_facturacion;
+CREATE DATABASE logifresh_transporte;
+CREATE DATABASE logifresh_notificaciones;
